d58dee8a8100cb5276eb8980f6cbdb865f36d06a
Cristina <crisalonso14@gmail.com>
2020-12-01 19:47:28 +0100

    tuppercss

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { MDBCol } from "mdbreact";
 
-function App() {
+import {
+  Container,
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+
+} from "reactstrap";
+
+import {
+  BrowserRouter
+} from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Tupper from "./components/Tupper";
+
+
+
+const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+    <BrowserRouter>
+      <Container fluid>
+
+
+        <Navbar color="rgb(255,204,153,0.5)" light expand="md">
+          <NavbarBrand href="/">TUKTOK</NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="ml-auto" navbar  >
+              <i className="fa fa-plus-circle fa-2x" aria-hidden="true" ></i>
+            </Nav>
+
+            <Nav className="ml-auto" navbar >
+              <MDBCol md="-4">
+                <input className="form-control" type="text" placeholder="Search" aria-label="Search" />
+              </MDBCol>
+
+              <UncontrolledDropdown nav inNavbar className="text-left">
+                <DropdownToggle nav caret>  Usuario </DropdownToggle>
+                <DropdownMenu >
+
+                  <DropdownItem>      Ofertas </DropdownItem>
+                  <DropdownItem>   Mis tuppers </DropdownItem>
+                  <DropdownItem>  Credito </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem> Editar   </DropdownItem>
+                  <DropdownItem> Cerrar sesión  </DropdownItem>
+
+
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+
+          </Collapse>
+        </Navbar>
+        <Tupper />
+      </Container>
+    </BrowserRouter>
+  );
+
+}
 export default App;
---
+++ app/src/components/Tupper.js
@@ -0,0 +1,64 @@
+import React from "react";
+import {
+    Container, Row
+} from "reactstrap";
+import './tupper.css';
+
+
+const Tupper = () => {
+    return (
+        <Container fluid className="prueba" >
+            <Row style={{paddingTop:"30px"}} className="w-100">
+                <div className="box col-12 col-sm-6 col-lg-4">
+                <div className="foto">
+                    <img src="https://s1.eestatic.com/2019/04/21/cocinillas/actualidad-gastronomica/Actualidad_gastronomica_392722143_120971228_1280x1280.jpg" width="100%" alt="Imagen actualidad gastronomica" /> 
+                </div>
+                <div className="info text-center">
+                    <div className="title">
+                        CROQUETAS
+                    </div>
+                    <div className="description font-bold" >
+                        corquetas de jamon hechas con mucho amor jsjjs jsjsjs sjjsjs sjsjjsjss nabsabjF JSDFLJSH FSDKFB LKJSDBLKJSD JSDHBF 
+                    </div>
+                    <div className="usuario font-bold" >
+                        usuario    
+                        <i className="fa fa-star" aria-hidden="true"></i> 
+                        <i className="fa fa-star" aria-hidden="true"></i>     
+                        <i className="fa fa-star" aria-hidden="true"></i>     
+                        <i className="fa fa-star-o" aria-hidden="true"></i>     
+                        <i className="fa fa-star-o" aria-hidden="true"></i>               
+                    </div>
+
+                 </div>
+                </div>
+               
+
+                
+                
+               
+                
+            </Row>
+            
+
+
+         
+
+
+
+
+
+
+        </Container >
+
+
+
+
+
+    );
+
+
+
+
+}
+
+export default Tupper;
