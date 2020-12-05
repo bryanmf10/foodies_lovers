965ebfa5f3df9a08de1d282e5d2de92076667cee
Cristina <crisalonso14@gmail.com>
2020-12-05 13:19:29 +0100

    Commit

---
+++ app/src/components/SubirTupper.js
@@ -0,0 +1,150 @@
+import {
+    Container, Row, Input, FormGroup, Col, Form, Label, CustomInput, Button, FormText
+} from "reactstrap";
+
+import './Subir.css';
+
+import styled from "styled-components";
+
+const Foto = styled.div`
+    width: 200px;
+    height: 200px;
+    display: flex;
+    background-image: url(${props => props.imagSrc});
+    background-size: cover;
+    background-position: center;
+    margin-left:47px;
+    
+    
+   
+`;
+
+
+
+
+const SubirTupper = (props) => {
+
+
+
+    return (
+        <>
+            <Container fluid style={{ backgroundColor: "rgb(249,208,127,0.2)" }}>
+
+                <Form >
+                    <Col className="col-sm-12 col-md-6  offset-md-3  cuerpoRegistro tituloSubirTupper" >
+
+                        ¿Que quieres vender?
+
+
+                    </Col>
+                    <Row form >
+                        <Col className=" col-sm-12 col-md-3  offset-md-3  cuerpoRegistro" >
+
+                            <Foto imagSrc="https://estaticos.miarevista.es/media/cache/760x570_thumb/uploads/images/recipe/5d5d03125bafe8dc49b479ab/ensalada-de-aguacates-int.jpg"></Foto>
+                            <FormGroup>
+                                <Label for="exampleCheckbox"> </Label>
+                                <div>
+                                    <CustomInput type="radio" id="exampleCustomRadio2" name="customRadio" label="1 $" inline />
+                                    <CustomInput type="radio" id="exampleCustomRadio2" name="customRadio" label="2 $" inline />
+                                    <CustomInput type="radio" id="exampleCustomRadio3" name="customRadio" label="  3 $" inline />
+                                    <CustomInput type="radio" id="exampleCustomRadio4" name="customRadio" label="Solo acepto Tupper" inline />
+                                </div>
+                            </FormGroup>
+                            <FormGroup>
+
+                                <CustomInput type="file" id="exampleCustomFileBrowser" name="customFile" label="Yo, pick a file!" />
+                            </FormGroup>
+
+
+
+                        </Col>
+
+
+                        <Col className="  col-sm-12 col-md-3  cuerpoRegistro "   >
+                            <FormGroup >
+                                <Label for="exampleText"  >Nombre</Label>
+
+                                <Input className="input" type="text" name="text" id="exampleText" placeholder="Introduce el nombre de tu Tupper" />
+
+                            </FormGroup>
+                            <FormGroup >
+                                <Label for="exampleText"  >Descripción</Label>
+
+                                <Input className="input" ype="textarea" name="text" id="exampleText" placeholder="Datos relevantes del tupper" maxLength="100" />
+
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="exampleCheckbox">...</Label>
+                                <div>
+                                    <CustomInput type="switch" id="exampleCustomSwitch" name="customSwitch" label="Vegano" />
+                                    <CustomInput type="switch" id="exampleCustomSwitch2" name="customSwitch" label="Vegetariano" />
+                                    <CustomInput type="switch" id="exampleCustomSwitch3" name="customSwitch" label=" Sin gluten" />
+                                    <CustomInput type="switch" id="exampleCustomSwitch4" name="customSwitch" label=" Sin lactosa" />
+                                    <CustomInput type="switch" id="exampleCustomSwitch5" name="customSwitch" label=" Sin frutos secos" />
+
+
+                                </div>
+                            </FormGroup>
+                        </Col>
+
+                    </Row>
+
+
+
+                    <Col className="col-sm-12 col-md-6  offset-md-3  cuerpoRegistro" >
+                        <Label for="exampleText" sm={2} >Ingredientes</Label>
+
+                        <FormGroup check >
+                            <Label check>
+                                <Input type="checkbox" /> Huevos
+                              </Label>
+                        </FormGroup>
+                        <FormGroup check >
+                            <Label check>
+                                <Input type="checkbox" /> Patata
+                             </Label>
+                        </FormGroup>
+                        <FormGroup check >
+                            <Label check>
+                                <Input type="checkbox" /> chocolate
+                             </Label>
+                        </FormGroup>
+                        <FormGroup check >
+                            <Label check>
+                                <Input type="checkbox" /> Pasta
+                             </Label>
+                        </FormGroup>
+                        <FormGroup check >
+                            <Label check>
+                                <Input type="checkbox" /> Sin frutos secos
+                             </Label>
+                        </FormGroup>
+                    </Col>
+
+
+
+
+
+                    <Col className="col-sm-12 col-md-6  offset-md-3  cuerpoRegistro" >
+
+                        <Button>Submit</Button>
+
+
+                    </Col>
+
+
+                </Form>
+
+
+            </Container>
+
+
+        </>
+    );
+
+
+
+
+}
+
+export default SubirTupper;
--- app/src/components/Tupper.js
+++ app/src/components/Tupper.js
@@ -7,8 +7,8 @@ import styled from "styled-components"
 
 
 const Foto = styled.div`
-    width: 300px;
-    height: 300px;
+    width: 200px;
+    height: 200px;
     display: inline-block;
     background-image: url(${props => props.imagSrc});
     background-size: cover;
@@ -20,56 +20,73 @@ const Box = styled.div`
     display: flex;
     flex-flow: column wrap;
     width: 100vw;
-    max-height: 100vh;
+    max-height: 150vh;
     justify-content: center;
     align-items: center;`;
 
 const Title = styled.div`
-    margin-Top: 30px;
+    margin-Top: 10px;
     font-Weight: bold; 
     font-Size: 20px;
-    font-weight: bold;
+    margin-left:10px;
 `;
 
 const Info = styled.div`
     font-family: Trispace, sans-serif;
     background-color: antiquewhite;
-    width: 300px;
-    text-align: center;
+    width: 200px;
+   
 `;
 
-const Description=styled.div`
+const Description = styled.div`
     margin: 10px;
     font-Size: 13px; 
     text-align: justify;
     font-weight: bold;
+    overflow:scroll; 
+    height:80px;
+
 
 `;
 
-const Usuario=styled.div`
+const Usuario = styled.div`
     text-Align: right;
-    margin: 10px;
-    margin:10px; 
+    margin:5px; 
     font-Size: 13px;
     scroll-padding-block:30px,
 
 `;
 
+const Divider=styled.div`
+border-left: 1px solid black;
+;
+`;
+
+const Botones=styled.div`
+    
+    display:flex;
+    justify-Content:space-around;
+    margin-Bottom:20px;
+
+
+
+`;
+
+
+
 
 const Tupper = () => {
     return (
-        <Container fluid  style={{backgroundColor:"rgb(249,208,127,0.2)"}}>
+        <Container fluid style={{ backgroundColor: "rgb(249,208,127,0.2)" }}>
             <Row style={{ paddingTop: "30px" }} className="w-100">
                 <Box className=" col-12 col-sm-6 col-lg-4">
                     <Foto imagSrc="https://s1.eestatic.com/2019/04/21/cocinillas/actualidad-gastronomica/Actualidad_gastronomica_392722143_120971228_1280x1280.jpg">
+
                     </Foto>
                     <Info>
                         <Title >
                             CROQUETAS
                         </Title>
-                        <Description>
-                            croquetas de jamon hechas con mucho amor jsjjs jsjsjs sjjsjs sjsjjsjss nabsabjF JSDFLJSH FSDKFB LKJSDBLKJSD JSDHBF
-                    </Description>
                         <Usuario >
                             Usuario
                         <i className="fa fa-star" aria-hidden="true"></i>
@@ -78,8 +95,26 @@ const Tupper = () => {
                             <i className="fa fa-star-o" aria-hidden="true"></i>
                             <i className="fa fa-star-o" aria-hidden="true"></i>
                         </Usuario>
+                        <Description>
+                            croquetas de jamon hechas con mucho amor  jvvl hgbjsjjs jsjsjs sjjsjs sjsjjsjss nabsabjF JSDFLJSH FSDKFB LKJSDBLKJSD JSDHBF
+                        </Description>
+                       
+                        <Botones>
+                            <i class="fa fa-info fa-2x" aria-hidden="true"></i>
+
+                            <Divider></Divider>
+                      
+
+                            <i class="fa fa-heart fa-2x" aria-hidden="true"></i>
+                       
+                     
+                           
+                        
+                        </Botones>
+
 
                     </Info>
+
                 </Box>
                 <Box className=" col-12 col-sm-6 col-lg-4">
                     <Foto imagSrc="https://estaticos.miarevista.es/media/cache/760x570_thumb/uploads/images/recipe/5d5d03125bafe8dc49b479ab/ensalada-de-aguacates-int.jpg">
