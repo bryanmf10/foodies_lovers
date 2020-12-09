324a332ecd7552630c0dbccc33098e6d0aaa4077
PlaMolina <platiden0@gmail.com>
2020-12-09 13:23:32 +0100

    detalle en proceso

--- app/src/components/Detalle.jsx
+++ app/src/components/Detalle.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import styled from 'styled-components';
 import imagen from './Profile/images/images.jpg';
 
-import Perfil from "./Profile/Perfil";
 
 
 const Chip = styled.div`
@@ -59,13 +58,32 @@ export default () => {
                 </Cabecera>
                 <Row>
                     <Col sm='12' lg='6' className="order-2 order-lg-1">
-                        <div style={{border: "1px solid black"}}>
-                            <img src={imagen} />
+                        <div style={{ border: "1px solid black" }}>
+                            <img src="https://estaticos.miarevista.es/media/cache/1140x_thumb/uploads/images/gallery/59b648f65bafe80ec7221fab/tupperinterior.jpg" style={{ width: "300px" }} />
+                        </div>
+                        <hr/>
+                        <div>
+                            <h3>INTOLERANCIAS</h3>
+                            <div>
+                                <i className="fas fa-circle"></i>
+                                <i className="fas fa-circle"></i>
+                                <i className="fas fa-circle"></i>
+                            </div>
+                            <h3>INGREDIENTES</h3>
+                            <ul>
+                                <li>limon</li>
+                                <li>lechuga</li>
+                                <li>tomate cherri</li>
+                            </ul>
                         </div>
                     </Col>
                     <Col sm='12' lg='6' className="order-1 order-lg-2">
-                        <div style={{border: "1px solid black"}}>
-                            <img src="https://www.w3schools.com/css/paris.jpg"  />
+                        <div style={{ border: "1px solid black" }}>
+                            <img src="https://i.blogs.es/b4dd5c/maps/1366_2000.png" style={{ width: "300px" }} />
+                        </div>
+                        <hr/>
+                        <div>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur quo, corrupti praesentium iure odit in veritatis incidunt aperiam atque ea at culpa hic nostrum sint reprehenderit optio sapiente impedit vero.</p>
                         </div>
                     </Col>
                 </Row>
